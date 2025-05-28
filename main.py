40d06f89395680773cb720978b692fc65cb1d2c7
PinkGran <96303309+PinkGran@users.noreply.github.com>
2025-05-28 17:20:41 +0400

    Add files via upload
    Hirst Painting Project

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
+from turtle import Turtle, Screen
+import random
 
-question_bank = []  #create an empty list
-for x in question_data:
-    question_text = x["text"]
-    question_answer = x["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+color_list = [(195, 177, 148), (159, 186, 177), (165, 184, 193), (198, 170, 180), (211, 196, 145), (205, 185, 184),
+              (196, 204, 200), (221, 213, 215), (201, 186, 189), (185, 195, 192), (194, 198, 200), (187, 193, 195),
+              (191, 191, 194), (131, 123, 112), (135, 129, 116), (137, 125, 120)]
 
-quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
-    quiz.question_number += 1
+tim = Turtle()
+tim.shape("classic")
+tim.color("black")
+tim.hideturtle()
+screen = Screen()
+screen.colormode(255)
+tim.speed(0)
+tim.pu()
+tim.setpos(-200, -200)
+tim.pd()
+y = 0
+for j in range(10):
+    for i in range(10):
+        y = tim.ycor()
+        tim.pd()
+        tim.dot(20, random.choice(color_list))
+        tim.pu()
+        tim.forward(50)
+        tim.pd()
+    tim.pu()
+    y = y + 50
+    tim.setpos(-200, y)
 
-print("You have completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+screen.exitonclick()
